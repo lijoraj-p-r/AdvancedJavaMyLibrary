5ff9c1a6b45cdd3ca662e9aa59c4c592373912fb
Lijoraj.p.r <105105861+lijoraj-p-r@users.noreply.github.com>
2025-09-13 12:07:19 +0530

    Create StringConcatenationTutorial.javaDay2

---
+++ javaBasic/StringConcatenationTutorial.java
@@ -0,0 +1,18 @@
+public class StringConcatenationTutorial {
+
+    public static void main(String[] args) {
+        // Example 1: Simple Concatenation
+        var name = "Priya";  // Storing a name
+        var message = name + " will become a software engineer!";
+        System.out.println(message);
+
+        // Separator for clarity in output
+        System.out.println();
+
+        // Example 2: Concatenating Multiple Variables
+        var firstName = "Rahul";
+        var lastName = "Sharma";
+        var fullMessage = firstName + " " + lastName + " is learning Java.";
+        System.out.println(fullMessage);
+    }
+}
