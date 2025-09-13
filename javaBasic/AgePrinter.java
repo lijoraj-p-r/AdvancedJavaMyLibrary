0b2829da66ad4a736ba5423dc233c727d64c3d54
Lijoraj.p.r <105105861+lijoraj-p-r@users.noreply.github.com>
2025-09-13 12:04:19 +0530

    Create AgePrinter.javaDay2

---
+++ javaBasic/AgePrinter.java
@@ -0,0 +1,6 @@
+public class AgePrinter {
+    public static void main(String[] args) {
+        var age = 23;  // Declare and initialize variable using var
+        System.out.println(age);  // Print the value to the console
+    }
+}
