42df5c392897c22441a59f960b2e869822b378e5
Lijoraj.p.r <105105861+lijoraj-p-r@users.noreply.github.com>
2025-09-17 20:01:20 +0530

    Create NumberCheckUsingIfElse.javaday5

---
+++ Programming Essentials/NumberCheckUsingIfElse.java
@@ -0,0 +1,16 @@
+// NumberCheckUsingIfElse.java
+// This program checks whether a number is positive, negative, or zero using if-else statements
+
+public class NumberCheckUsingIfElse {
+    public static void main(String[] args) {
+        int number = 10;  // Change this value to test different numbers
+
+        if (number > 0) {
+            System.out.println(number + " is positive.");
+        } else if (number < 0) {
+            System.out.println(number + " is negative.");
+        } else {
+            System.out.println("The number is zero.");
+        }
+    }
+}
