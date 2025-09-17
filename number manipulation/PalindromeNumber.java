b4b8d5fdbbfb8ebe83ee02be4d862e51f3f0eefb
Lijoraj.p.r <105105861+lijoraj-p-r@users.noreply.github.com>
2025-09-17 20:18:27 +0530

    Create PalindromeNumber.java

---
+++ number manipulation/PalindromeNumber.java
@@ -0,0 +1,22 @@
+// PalindromeNumber.java
+// Checks if a number is a palindrome
+
+public class PalindromeNumber {
+    public static void main(String[] args) {
+        int number = 12321;
+        int original = number;
+        int reversed = 0;
+
+        while (number != 0) {
+            int digit = number % 10;
+            reversed = reversed * 10 + digit;
+            number /= 10;
+        }
+
+        if (original == reversed) {
+            System.out.println(original + " is a palindrome.");
+        } else {
+            System.out.println(original + " is not a palindrome.");
+        }
+    }
+}
